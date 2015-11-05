458d2fb84fd051f37f1250b3739cfd7488023028
Dmitri Belinski <belinski.d@gmail.com>
2015-11-05 20:16:10 +0200

    init database, make @db (before do)

--- app.rb
+++ app.rb
@@ -2,6 +2,31 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash = true
+end
+
+before do
+	
+end
+
+# configure do
+# 	db = get_db 										#
+# 	db.execute 'CREATE TABLE IF NOT EXISTS 						
+# 			"Content" 
+# 			(
+# 				"id" INTEGER PRIMARY KEY AUTOINCREMENT, 
+# 				"username" TEXT, 
+# 				"phone" TEXT, 
+# 				"datestamp" TEXT, 
+# 				"barber" TEXT, 
+# 				"color" TEXT
+# 			)'	
+# end
+
 
 get '/' do
 	erb "Hello!</a>"			
